85c7e71a894402e7af1b67caee62bd52d1228d94
veekshagokulms <83774448+veekshagokulms@users.noreply.github.com>
2021-05-12 21:12:15 +0530

    Add files via upload

---
+++ POSITIVE NUM BERS.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# list of numbers
+list1 = [12, -7, 5, 64,-14]
+ 
+# iterating each number in list
+for num in list1:
+     
+    # checking condition
+    if num >= 0:
+       print(num, end = " ")
+
+
+# In[2]:
+
+
+# list of numbers
+list1 = [12, 14,-95, 3]
+ 
+# iterating each number in list
+for num in list1:
+     
+    # checking condition
+    if num >= 0:
+       print(num, end = " ")
+
+
+# In[ ]:
+
+
+
+
